cec41eaa0f2d1272a577d902a4aec8f1dcd5e658
gpoon626 <gpoon626@gmail.com>
2020-06-29 15:24:46 -0400

    Curl-scripts
    movieRoutes
    
    movieSchema
    
    server.js

---
+++ app/models/movie.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+
+const movieSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  rating: {
+    type: Number,
+    required: true
+  }
+}, {
+  timestamps: true
+})
+module.exports = mongoose.model('Movie', movieSchema)
---
+++ app/routes/movieRoutes.js
@@ -0,0 +1,60 @@
+// const mongoose = require('mongoose')
+// mongoose.Promise = global.Promise
+const express = require('express')
+const router = express.Router()
+// require book models
+const Movie = require('./../models/movie.js')
+const handle404 = require('../../lib/custom_errors')
+
+// movie create route
+// CREATE
+// POST /movies/
+router.post('/movies', (req, res, next) => {
+  // get the data from the request object ?
+  const movieData = req.body.movie
+  Movie.create(movieData)
+    .then(movie => res.status(201).json({movie: movie}))
+    .catch(next)
+})
+
+// INDEX
+// GET /movies
+router.get('/movies', (req, res, next) => {
+  Movie.find()
+    .then(movie => res.json({movie: movie}))
+    .catch(next)
+})
+
+// SHOW
+// GET /movies/:id
+router.get('/movies/:id', (req, res, next) => {
+  const id = req.params.id
+  Movie.findById(id)
+    .then(movie => res.json({movie: movie}))
+    .catch(next)
+})
+
+// UPDATE
+// PATCH /movies/:id
+router.patch('/movies/:id', (req, res, next) => {
+  const id = req.params.id
+  const movieData = req.body.movie
+  Movie.findById(id)
+    .then(handle404)
+    .then(movie => movie.update(movieData))
+    .then(() => res.sendStatus(204))
+    .catch(next)
+})
+
+// DESTROY
+// DELETE /movies/:id
+router.delete('/movies/:id', (req, res, next) => {
+  const id = req.params.id
+  Movie.findById(id)
+    .then(handle404)
+    .then(movie => movie.remove())
+    .then(() => res.sendStatus(204))
+    .catch(next)
+})
+
+module.exports = router
